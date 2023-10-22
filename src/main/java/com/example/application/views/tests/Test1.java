a1d31b59ccaed0a88ef044abc741c8c585eca883
Danecka <obzornik12@gmail.com>
2023-10-22 23:12:25 +0300

    улучшение тестов, добавлен класс с компонентоми тестов. Теперь повляются вопросы и варинаты ответов.

--- src/main/java/com/example/application/views/tests/Test1.java
+++ src/main/java/com/example/application/views/tests/Test1.java
@@ -29,14 +29,50 @@ public class Test1 extends Composite<VerticalLayout> {
     Map <Integer, String> chosenOptions = new HashMap<>();
     static String option;
     static int i = 1;
-    static int numberOfQ = 15;
+    static int numberOfQ = 5;
+
+
+    static Map<Integer, Integer> typeQ = new HashMap<>();
+    static {
+        for (int j = 1; j <= 5; j++) {
+            typeQ.put(j, 1);
+        }
+    }
+    static Map<Integer, String> questions = new HashMap<>();
+    static {
+        questions.put(1, "Какого цвета солнце");
+        questions.put(2, "Сколько углов у квадрата");
+        questions.put(3, "Какой сейчас год");
+        questions.put(4, "Зимой и летом одним цветом");
+        questions.put(5, "Тяжело в учении легко в ...");
+    }
+    static Map<Integer, String[]> answers = new HashMap();
+    static {
+        answers.put(1, new String[]{"красное", "голубое","жёлтое","Зелёное"});
+        answers.put(2, new String[]{"один", "два","три","четыре"});
+        answers.put(3, new String[]{"1000", "2000","2023","2007"});
+        answers.put(4, new String[]{"Ёлка", "Подснежник","Принтер","Мухамор"});
+        answers.put(5, new String[]{"Мучении", "Нраваучении","Сожалении","Бою"});
+    }
+
+    static Map<Integer,Integer> answerTrue = new HashMap<>();
+    static {
+        answerTrue.put(1, 3);
+        answerTrue.put(2, 4);
+        answerTrue.put(3, 3);
+        answerTrue.put(4, 1);
+        answerTrue.put(5, 4);
+    }
+
+    RadioButtonGroup radioGroup = new RadioButtonGroup();
+    TestComponents tc = new TestComponents(typeQ, questions, answers, answerTrue);
 
 
     public Test1() {
 
 
         H1 h1 = new H1();
-        RadioButtonGroup radioGroup = new RadioButtonGroup();
+
         HorizontalLayout layoutRow = new HorizontalLayout();
         Button nextButton = new Button();
         Button previousButton = new Button();
@@ -45,9 +81,9 @@ public class Test1 extends Composite<VerticalLayout> {
         getContent().setHeightFull();
         getContent().setWidthFull();
 
-        h1.setText(i + "/15");
-        radioGroup.setLabel("Radio Group");
-        radioGroup.setItems("Order ID", "Product Name", "Customer", "Status");
+        h1.setText(i + "/" + numberOfQ);
+        radioGroup.setLabel(questions.get(i));
+        radioGroup.setItems(tc.answers.get(i));
         radioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
         layoutRow.setWidthFull();
         getContent().setFlexGrow(1.0, layoutRow);
@@ -101,12 +137,14 @@ public class Test1 extends Composite<VerticalLayout> {
         });
     }
     private void nextQuestion(RadioButtonGroup radioGroup){
-        radioGroup.setItems("Order ID", "Product Name", "Customer", "Status");
+        radioGroup.setLabel(questions.get(i));
+        radioGroup.setItems(tc.answers.get(i));
         choiceNotifier(radioGroup);
     }
 
     private void previousQuestion(RadioButtonGroup radioGroup){
-        radioGroup.setItems("Order ID", "Product Name", "Customer", "Status");
+        radioGroup.setLabel(questions.get(i));
+        radioGroup.setItems(tc.answers.get(i));
         choiceNotifier(radioGroup);
 
     }
---
+++ src/main/java/com/example/application/views/tests/TestComponents.java
@@ -0,0 +1,36 @@
+package com.example.application.views.tests;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestComponents {
+    Map<Integer, Integer> typeQ;
+    Map<Integer, String> questions;
+    Map<Integer, String[]> answers;
+    Map<Integer,Integer> answerTrue;
+    RadioButtonGroup radioGroup;
+    HorizontalLayout layoutRow;
+    HorizontalLayout layoutRow2;
+    HorizontalLayout layoutRow3;
+
+    public TestComponents(Map<Integer, Integer> typeQ, Map<Integer, String> questions, Map<Integer, String[]> answers, Map<Integer, Integer> answerTrue, RadioButtonGroup radioGroup, HorizontalLayout layoutRow, HorizontalLayout layoutRow2, HorizontalLayout layoutRow3) {
+        this.typeQ = typeQ;
+        this.questions = questions;
+        this.answers = answers;
+        this.answerTrue = answerTrue;
+        this.radioGroup = radioGroup;
+        this.layoutRow = layoutRow;
+        this.layoutRow2 = layoutRow2;
+        this.layoutRow3 = layoutRow3;
+    }
+
+    public TestComponents(Map<Integer, Integer> typeQ, Map<Integer, String> questions, Map<Integer, String[]> answers, Map<Integer, Integer> answerTrue) {
+        this.typeQ = typeQ;
+        this.questions = questions;
+        this.answers = answers;
+        this.answerTrue = answerTrue;
+    }
+}
