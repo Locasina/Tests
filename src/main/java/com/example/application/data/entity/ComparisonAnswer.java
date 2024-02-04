5690ebb3157485317c9d37538c37acc3e443fd18
Logo Pedic <kolakiruhin@gmail.com>
2024-02-04 19:14:12 +0300

    Сделаны гриды

--- src/main/java/com/example/application/data/entity/ComparisonAnswer.java
+++ src/main/java/com/example/application/data/entity/ComparisonAnswer.java
@@ -9,12 +9,16 @@ public class ComparisonAnswer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "question_id")
     private Question question;
 
 
     private String column1;
+
     private String column2;
+
+
 
 }
